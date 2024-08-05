b38e9c4627149af18e1b13c84bdaccf31089fde2
DESKTOP-0EKJRMC\neurois <suzuki.y.da@gmail.com>
2024-08-06 06:12:53 +0900

    タッチストーンビューアをランチャーに追加

--- NyanPy/__main__.py
+++ NyanPy/__main__.py
@@ -6,9 +6,13 @@ import os
 
 QtCore.QDir.addSearchPath('img', os.path.join(os.path.dirname(__file__), 'Resources/Images'))
 
+# Main Applications
 from AnalogClockPackage.AnalogClockModule import AnalogClock
 from CodeEditorPackage.CodeEditorWindowModule import CodeEditorWindow
 from SheetEditorPackage.SheetEditorModule import SheetEditor
+
+# Microwave RF
+from MicrowaveRFPackage.TouchstoneViewerPackage.TouchstoneViewerModule import TouchstoneViewer
 
 class Launcher(QtWidgets.QMainWindow):
 
@@ -27,6 +31,10 @@ class Launcher(QtWidgets.QMainWindow):
         apps.addChild(QtWidgets.QTreeWidgetItem(['CodeEditor' , 'Text editor for coding Python and Matlab/GNU Octave']))
         apps.addChild(QtWidgets.QTreeWidgetItem(['SheetEditor', 'Spreadsheet for Vimmers']))
         tableWidget.addTopLevelItem(apps)
+
+        microwaveRF = QtWidgets.QTreeWidgetItem(['Microwave RF'])
+        microwaveRF.addChild(QtWidgets.QTreeWidgetItem(['TouchstoneViewer', 'Viewer for Touchstone (SnP) files']))
+        tableWidget.addTopLevelItem(microwaveRF)
 
         tableWidget.expandAll()
         tableWidget.header().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
@@ -57,6 +65,7 @@ class Launcher(QtWidgets.QMainWindow):
         app = AnalogClock() if name == 'AnalogClock'\
                 else CodeEditorWindow() if name == 'CodeEditor'\
                 else SheetEditor() if name == 'SheetEditor'\
+                else TouchstoneViewer() if name == 'TouchstoneViewer'\
                 else None
         if app:
             self.__children.append(app)
